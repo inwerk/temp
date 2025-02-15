286a4a98286cc0f2b771b63c60f0722206c91fa1
inwerk <142039445+inwerk@users.noreply.github.com>
2025-02-15 19:07:39 +0100

    test

--- setup.sh
+++ setup.sh
@@ -3,13 +3,14 @@ docker compose down
 # Import environment variables from .env file
 # set -a && source .env && set +a
 
-docker secret create NEXTCLOUD_MYSQL_ROOT_PASSWORD <(openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9)
-docker secret create NEXTCLOUD_MYSQL_PASSWORD <(openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9)
-docker secret create NEXTCLOUD_REDIS_HOST_PASSWORD <(openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9)
-
 mkdir -p /mnt/data
 mkdir -p /mnt/data/letsencrypt
 mkdir -p /mnt/data/nextcloud
 mkdir -p /mnt/data/mysql
+mkdir -p /mnt/data/secrets
+
+openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > /mnt/data/secrets/NEXTCLOUD_MYSQL_ROOT_PASSWORD
+openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > /mnt/data/secrets/NEXTCLOUD_MYSQL_PASSWORD
+openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > /mnt/data/secrets/NEXTCLOUD_REDIS_HOST_PASSWORD
 
 docker compose up -d
