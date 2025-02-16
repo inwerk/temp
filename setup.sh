e5afbd7c95101b9a85be5d792c98725e0728218a
inwerk <142039445+inwerk@users.noreply.github.com>
2025-02-16 18:21:21 +0100

    test

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@ docker compose down
 # Import environment variables from .env file
 # set -a && source .env && set +a
 
-useradd --no-create-home nobody --shell /usr/sbin/nologin --uid 2000
-
 mkdir -p /mnt/data
 mkdir -p /mnt/data/letsencrypt
 mkdir -p /mnt/data/nextcloud
 mkdir -p /mnt/data/mysql
 mkdir -p /mnt/data/secrets
+
+touch /mnt/data/remoteip.conf
 
 openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > /mnt/data/secrets/NEXTCLOUD_MYSQL_ROOT_PASSWORD
 openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > /mnt/data/secrets/NEXTCLOUD_MYSQL_PASSWORD
