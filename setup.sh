061db6680bf46ec14a27a6f2b060336534ab259f
inwerk <142039445+inwerk@users.noreply.github.com>
2025-02-16 17:50:57 +0100

    test

--- setup.sh
+++ setup.sh
@@ -9,8 +9,6 @@ mkdir -p /mnt/data/nextcloud
 mkdir -p /mnt/data/mysql
 mkdir -p /mnt/data/secrets
 
-chown -R 33:33 /mnt/data/nextcloud
-
 openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > /mnt/data/secrets/NEXTCLOUD_MYSQL_ROOT_PASSWORD
 openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > /mnt/data/secrets/NEXTCLOUD_MYSQL_PASSWORD
 openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > /mnt/data/secrets/NEXTCLOUD_REDIS_HOST_PASSWORD
