e0a8b68fde231fc6f8ea18f1551cf9d1c804cb13
inwerk <142039445+inwerk@users.noreply.github.com>
2025-02-17 17:28:30 +0100

    test

--- setup.sh
+++ setup.sh
@@ -5,14 +5,14 @@ docker compose down
 
 mkdir -p /mnt/data
 mkdir -p /mnt/data/letsencrypt
-mkdir -p /mnt/data/nextcloud && chown 33:33 /mnt/data/nextcloud
+mkdir -p /mnt/data/nextcloud && chown 2022:2022 /mnt/data/nextcloud
 mkdir -p /mnt/data/mysql && chown 2022:2022 /mnt/data/mysql
 mkdir -p /mnt/data/secrets
 
 # workaround for https://github.com/nextcloud/docker/issues/1494 and https://github.com/nextcloud/docker/issues/763
 mkdir -p /mnt/data/.fix
-touch /mnt/data/.fix/remoteip.conf && chown 33:33 /mnt/data/.fix/remoteip.conf
-touch /mnt/data/.fix/redis-session.ini && chown 33:33 /mnt/data/.fix/redis-session.ini
+touch /mnt/data/.fix/remoteip.conf && chown 2022:2022 /mnt/data/.fix/remoteip.conf
+touch /mnt/data/.fix/redis-session.ini && chown 2022:2022 /mnt/data/.fix/redis-session.ini
 
 openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > /mnt/data/secrets/NEXTCLOUD_MYSQL_ROOT_PASSWORD
 openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > /mnt/data/secrets/NEXTCLOUD_MYSQL_PASSWORD
