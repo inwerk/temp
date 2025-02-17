53512abf1caebf7c1c4f48033c43633dbd7b4f74
inwerk <142039445+inwerk@users.noreply.github.com>
2025-02-17 14:12:28 +0100

    test

--- setup.sh
+++ setup.sh
@@ -9,9 +9,9 @@ mkdir -p /mnt/data/nextcloud
 mkdir -p /mnt/data/mysql
 mkdir -p /mnt/data/secrets
 
-touch /mnt/data/remoteip.conf && chown 33:33 /mnt/data/remoteip.conf
-touch /mnt/data/redis-session.ini && chown 33:33 /mnt/data/redis-session.ini
-touch /mnt/data/nextcloud-init-sync.lock && chown 33:33 /mnt/data/nextcloud-init-sync.lock
+touch /mnt/data/.conf/remoteip.conf && chown 33:33 /mnt/data/.conf/remoteip.conf
+touch /mnt/data/.conf/redis-session.ini && chown 33:33 /mnt/.conf/data/redis-session.ini
+touch /mnt/data/.conf/nextcloud-init-sync.lock && chown 33:33 /mnt/data/.conf/nextcloud-init-sync.lock
 
 openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > /mnt/data/secrets/NEXTCLOUD_MYSQL_ROOT_PASSWORD
 openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > /mnt/data/secrets/NEXTCLOUD_MYSQL_PASSWORD
