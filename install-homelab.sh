6bd9595ee964ec58b3edf8c1312ba1b366dd96cc
inwerk <142039445+inwerk@users.noreply.github.com>
2025-02-19 19:16:47 +0100

    test

--- install-homelab.sh
+++ install-homelab.sh
@@ -1,8 +1,5 @@
 docker compose down
 
-# Import environment variables from config.env file
-set -a && source config.env && set +a
-
 mkdir -p /mnt/data
 mkdir -p /mnt/data/letsencrypt
 mkdir -p /mnt/data/nextcloud && chown 2022:2022 /mnt/data/nextcloud
@@ -14,8 +11,20 @@ mkdir -p /mnt/data/.fix
 touch /mnt/data/.fix/remoteip.conf && chown 2022:2022 /mnt/data/.fix/remoteip.conf
 touch /mnt/data/.fix/redis-session.ini && chown 2022:2022 /mnt/data/.fix/redis-session.ini
 
-openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > /mnt/data/secrets/NEXTCLOUD_MYSQL_ROOT_PASSWORD && chmod 644 /mnt/data/secrets/NEXTCLOUD_MYSQL_ROOT_PASSWORD
-openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > /mnt/data/secrets/NEXTCLOUD_MYSQL_PASSWORD && chmod 644 /mnt/data/secrets/NEXTCLOUD_MYSQL_PASSWORD
-openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > /mnt/data/secrets/NEXTCLOUD_REDIS_HOST_PASSWORD && chmod 644 /mnt/data/secrets/NEXTCLOUD_REDIS_HOST_PASSWORD
+# Import environment variables from config.env file
+set -a && source config.env && set +a
+
+openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > /mnt/data/secrets/NEXTCLOUD_MYSQL_ROOT_PASSWORD
+openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > /mnt/data/secrets/NEXTCLOUD_MYSQL_PASSWORD
+openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > /mnt/data/secrets/NEXTCLOUD_REDIS_HOST_PASSWORD
+
+echo "EMAIL=$EMAIL" > .env
+echo "DOMAIN=$DOMAIN" >> .env
+echo "CLOUDFLARE_DNS_API_TOKEN=$CLOUDFLARE_DNS_API_TOKEN" >> .env
+echo "NEXTCLOUD_REDIS_HOST_PASSWORD=$(cat /mnt/data/secrets/NEXTCLOUD_REDIS_HOST_PASSWORD)" >> .env
+echo "NEXTCLOUD_MYSQL_PASSWORD=$(cat /mnt/data/secrets/NEXTCLOUD_MYSQL_PASSWORD)" >> .env
+echo "NEXTCLOUD_MYSQL_ROOT_PASSWORD=$(cat /mnt/data/secrets/NEXTCLOUD_MYSQL_ROOT_PASSWORD)" >> .env
 
 docker compose up -d
+
+rm .env
