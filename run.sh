9a09b81c3a004bed756207b024059f2a6d844eba
inwerk <142039445+inwerk@users.noreply.github.com>
2025-06-26 11:32:33 +0200

    test

--- run.sh
+++ run.sh
@@ -1,19 +1,21 @@
 # Load environment variables
-export $(grep -v '^#' .env | xargs)
+set -a
+source .env
+set +a
 
 docker compose down
 
-mkdir -p ${VOLUME_PATH}
-mkdir -p ${VOLUME_PATH}/crowdsec
-mkdir -p ${VOLUME_PATH}/letsencrypt
-mkdir -p ${VOLUME_PATH}/nextcloud
-mkdir -p ${VOLUME_PATH}/nextcloud-database
-mkdir -p ${VOLUME_PATH}/nextcloud-redis
-mkdir -p ${VOLUME_PATH}/secrets
+mkdir -p ${DATA_PATH}
+mkdir -p ${DATA_PATH}/crowdsec
+mkdir -p ${DATA_PATH}/letsencrypt
+mkdir -p ${DATA_PATH}/nextcloud
+mkdir -p ${DATA_PATH}/nextcloud-database
+mkdir -p ${DATA_PATH}/nextcloud-redis
+mkdir -p ${DATA_PATH}/secrets
 
 # Create secrets
 generate_secret() {
-  local secret_path="${VOLUME_PATH}/secrets/$1"
+  local secret_path="${DATA_PATH}/secrets/$1"
   if [ ! -f "$secret_path" ]; then
     openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > "$secret_path"
   fi
