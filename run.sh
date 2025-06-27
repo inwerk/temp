de77b42839d8513100f6e39ccb0f24a92d3df7ef
inwerk <142039445+inwerk@users.noreply.github.com>
2025-06-27 15:45:05 +0200

    fix

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ chown www-data:www-data ${DATA_PATH}/nextcloud
 chown www-data:www-data ${DATA_PATH}/nextcloud/data
 chown www-data:www-data ${DATA_PATH}/nextcloud/data/nextcloud.log
 
-chmod a+x scripts/nextcloud-post-installation.sh
+chmod +x scripts/nextcloud-post-installation.sh
 
 # Create secrets
 generate_secret() {
