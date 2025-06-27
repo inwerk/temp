644f177397ba36b9fc0bae44f2715810638461c9
inwerk <142039445+inwerk@users.noreply.github.com>
2025-06-27 19:29:28 +0200

    fix

--- run.sh
+++ run.sh
@@ -3,23 +3,21 @@ set -a
 source .env
 set +a
 
-# Paths for persistent volumes
-export DATA_PATH=".docker/data"
-export SECRETS_PATH=".docker/secrets"
-export LOG_PATH=".docker/logs"
-
 mkdir -p .docker
 
-mkdir -p ${DATA_PATH}
-mkdir -p ${DATA_PATH}/crowdsec
-mkdir -p ${DATA_PATH}/letsencrypt
-mkdir -p ${DATA_PATH}/nextcloud
-mkdir -p ${DATA_PATH}/nextcloud-database
-mkdir -p ${DATA_PATH}/nextcloud-redis
-mkdir -p ${DATA_PATH}/secrets
+mkdir -p .docker/data
+mkdir -p .docker/data/crowdsec
+mkdir -p .docker/data/letsencrypt
+mkdir -p .docker/data/nextcloud
+mkdir -p .docker/data/nextcloud-database
+mkdir -p .docker/data/nextcloud-redis
+mkdir -p .docker/data/vaultwarden
+mkdir -p .docker/data/vaultwarden-database
 
-mkdir -p ${LOG_PATH}
-mkdir -p ${LOG_PATH}/nextcloud
+mkdir -p .docker/logs
+mkdir -p .docker/logs/nextcloud
+mkdir -p .docker/logs/traefik
+mkdir -p .docker/logs/vaultwarden
 
 # fix to mount nextcloud.log
 chown www-data:www-data ${LOG_PATH}/nextcloud
@@ -27,7 +25,7 @@ chown www-data:www-data ${LOG_PATH}/nextcloud
 chmod +x scripts/nextcloud-post-installation.sh
 chmod +x scripts/vaultwarden-entrypoint.sh
 
-mkdir -p ${SECRETS_PATH}
+mkdir -p .docker/secrets
 
 # Create secrets
 generate_secret() {
