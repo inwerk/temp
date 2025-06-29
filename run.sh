6abd21c39d3c39143c47a2eddb61b79333c9bf81
inwerk <142039445+inwerk@users.noreply.github.com>
2025-06-29 16:38:09 +0200

    feat: use docker volumes

--- run.sh
+++ run.sh
@@ -3,33 +3,15 @@ set -a
 source .env
 set +a
 
-mkdir -p .docker
-
-mkdir -p .docker/data
-mkdir -p .docker/data/crowdsec
-mkdir -p .docker/data/letsencrypt
-mkdir -p .docker/data/nextcloud
-mkdir -p .docker/data/nextcloud-database
-mkdir -p .docker/data/nextcloud-redis
-mkdir -p .docker/data/vaultwarden
-mkdir -p .docker/data/vaultwarden-database
-
-mkdir -p .docker/logs
-mkdir -p .docker/logs/nextcloud
-mkdir -p .docker/logs/traefik
-mkdir -p .docker/logs/vaultwarden
-
-# fix to mount nextcloud.log
-chown www-data:www-data ${LOG_PATH}/nextcloud
-
 chmod +x scripts/nextcloud-post-installation.sh
 chmod +x scripts/vaultwarden-entrypoint.sh
 
+mkdir -p .docker
 mkdir -p .docker/secrets
 
 # Create secrets
 generate_secret() {
-  local secret_path="${SECRETS_PATH}/$1"
+  local secret_path=".docker/secrets/$1"
   if [ ! -f "$secret_path" ]; then
     openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > "$secret_path"
   fi
