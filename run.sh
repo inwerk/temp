d64f6952a7f37ceb9994721b81bb93b1eca162d3
inwerk <142039445+inwerk@users.noreply.github.com>
2025-07-11 18:32:09 +0200

    feat: improve secrets

--- run.sh
+++ run.sh
@@ -1,23 +1,64 @@
-# Load environment variables
-set -a
-source .env
-set +a
+#!/usr/bin/env bash
 
-mkdir -p .docker
-mkdir -p .docker/secrets
+set -euo pipefail
+
+# Load environment variables
+if [[ -f .env ]]; then
+  set -a
+  source .env
+  set +a
+else
+  echo "Error: .env file not found." >&2
+  exit 1
+fi
+
+# Check for openssl
+if ! command -v openssl >/dev/null 2>&1; then
+  echo "Error: openssl is not installed. Please install it (sudo apt update && sudo apt install openssl) and try again." >&2
+  exit 1
+fi
+
+# Check for argon2
+if ! command -v argon2 >/dev/null 2>&1; then
+  echo "Error: openssl is not installed. Please install it (sudo apt update && sudo apt install argon2) and try again." >&2
+  exit 1
+fi
 
 # Create secrets
+mkdir -p .docker/secrets
+
 generate_secret() {
-  local secret_path=".docker/secrets/$1"
-  if [ ! -f "$secret_path" ]; then
-    openssl rand -base64 32 | tr -dc _A-Z-a-z-0-9 > "$secret_path"
+  local secret_name="$1"
+  local secret_path=".docker/secrets/$secret_name"
+  if [[ ! -f "$secret_path" ]]; then
+    openssl rand -base64 32 | tr -dc 'A-Za-z0-9' > "$secret_path"
+    chmod 600 "$secret_path"
+    echo "Secret $secret_name created."
+  else
+    echo "Secret $secret_name already exists."
   fi
 }
 
-generate_secret "NEXTCLOUD_ADMIN_PASSWORD"
-generate_secret "NEXTCLOUD_POSTGRES_PASSWORD"
-generate_secret "NEXTCLOUD_REDIS_HOST_PASSWORD"
-generate_secret "VAULTWARDEN_ADMIN_TOKEN"
-generate_secret "VAULTWARDEN_POSTGRES_PASSWORD"
+secrets=(
+  "NEXTCLOUD_ADMIN_PASSWORD"
+  "NEXTCLOUD_POSTGRES_PASSWORD"
+  "NEXTCLOUD_REDIS_HOST_PASSWORD"
+  "VAULTWARDEN_ADMIN_TOKEN"
+  "VAULTWARDEN_POSTGRES_PASSWORD"
+)
 
+for secret in "${secrets[@]}"; do
+  generate_secret "$secret"
+done
+
+# Create a secure hash of the VAULTWARDEN_ADMIN_TOKEN
+if [[ ! -f ".docker/secrets/VAULTWARDEN_ADMIN_TOKEN_HASH" ]]; then
+  argon2 "$(openssl rand -base64 32)" -e -id -k 19456 -t 2 -p 1 < .docker/secrets/VAULTWARDEN_ADMIN_TOKEN | sed 's#\$#\$\$#g' > ".docker/secrets/VAULTWARDEN_ADMIN_TOKEN_HASH"
+  chmod 600 ".docker/secrets/VAULTWARDEN_ADMIN_TOKEN_HASH"
+  echo "VAULTWARDEN_ADMIN_TOKEN_HASH created."
+else
+  echo "VAULTWARDEN_ADMIN_TOKEN_HASH already exists."
+fi
+
+# Deploy with docker compose
 docker compose up -d
--- vaultwarden/vaultwarden-entrypoint.sh
+++ vaultwarden/vaultwarden-entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 # Read the Postgres password from the Docker secret
 export POSTGRES_PASSWORD=$(cat /run/secrets/VAULTWARDEN_POSTGRES_PASSWORD)
-export DATABASE_URL="postgresql://vaultwarden:${POSTGRES_PASSWORD}@vaultwarden-database/vaultwarden"
+export DATABASE_URL="postgresql://vaultwarden:${POSTGRES_PASSWORD}@vaultwarden-db/vaultwarden"
 
 exec /start.sh
